00e2d7d376879becada0278444272afa45c01c45
lisa1130j <70610155+lisa1130j@users.noreply.github.com>
2020-09-01 17:29:19 -0700

    restructuring it the correct way from the project.

---
+++ Employee.Application/Service/IEmployeeService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Employee.Application.Service
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Application.DTO.Employee> GetAllEmployees();
+        int AddEmployee(Application.DTO.Employee employee);
+    }
+}
