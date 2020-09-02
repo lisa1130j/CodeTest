3b2539842aec7e63e5dfbdb6549d77241b663065
lisa1130j <70610155+lisa1130j@users.noreply.github.com>
2020-09-01 17:28:28 -0700

    Add files via upload

---
+++ Query/IEmployeeQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Employee.Application.Query
+{
+    public interface IEmployeeQuery
+    {
+        IEnumerable<DTO.Employee> GetEmployees();
+    }
+}
